3f909ffc28bc43a6970ee08419e55db2c35299e1
Can Kuloğlu <can.kuloglu@bahcesehir.edu.tr>
2025-05-08 16:48:59 +0300

    NavigationGraph for navigating among the screens and sending data

---
+++ app/src/main/java/com/example/foodapp/presentation/navigation/NavGraph.kt
@@ -0,0 +1,36 @@
+package com.example.foodapp.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.foodapp.presentation.detail.DetailScreen
+import com.example.foodapp.presentation.home.HomeScreen
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    modifier: Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = "home") {
+
+        composable("home") {
+            HomeScreen(
+                modifier = modifier,
+                onRecipeClick = { recipeId ->
+                    navController.navigate("detail/$recipeId")
+                }
+            )
+        }
+
+        composable("detail/{recipeId}") { backStackEntry ->
+            val recipeId = backStackEntry.arguments?.getString("recipeId")?.toIntOrNull()
+            recipeId?.let {
+                DetailScreen(recipeId = it, modifier = modifier)
+            }
+        }
+    }
+}

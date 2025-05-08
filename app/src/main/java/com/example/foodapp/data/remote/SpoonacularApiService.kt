361d1710f45cf99ab50726e1b3016576bd16359e
Can Kuloğlu <can.kuloglu@bahcesehir.edu.tr>
2025-05-08 16:56:51 +0300

    Implemented the API Endpoint for getting the recipe details

--- app/src/main/java/com/example/foodapp/data/remote/SpoonacularApiService.kt
+++ app/src/main/java/com/example/foodapp/data/remote/SpoonacularApiService.kt
@@ -1,8 +1,10 @@
 package com.example.foodapp.data.remote
 
 import com.example.foodapp.data.dto.RandomRecipeResponseDto
+import com.example.foodapp.data.dto.RecipeDetailResponseDto
 import com.example.foodapp.data.dto.SearchRecipeResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SpoonacularApiService {
@@ -18,4 +20,11 @@ interface SpoonacularApiService {
         @Query("query") query: String,
         @Query("apiKey") apiKey: String
     ): SearchRecipeResponseDto
+
+    @GET("recipes/{id}/information")
+    suspend fun getRecipeInformation(
+        @Path("id") id: Int,
+        @Query("apiKey") apiKey: String,
+        @Query("includeNutrition") includeNutrition: Boolean = false
+    ): RecipeDetailResponseDto
 }

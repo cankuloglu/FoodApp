88d3ce8934706ffbfd639299355db70fff27b276
Can Kuloğlu <can.kuloglu@bahcesehir.edu.tr>
2025-05-08 16:54:26 +0300

    Added the suspend function for getting recipe details

--- app/src/main/java/com/example/foodapp/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/foodapp/domain/repository/RecipeRepository.kt
@@ -1,9 +1,11 @@
 package com.example.foodapp.domain.repository
 
 import com.example.foodapp.domain.model.Recipe
+import com.example.foodapp.domain.model.RecipeDetail
 
 
 interface RecipeRepository {
     suspend fun getRandomRecipes(): List<Recipe>
     suspend fun searchRecipes(query: String): List<Recipe>
+    suspend fun getRecipeDetail(id:Int): RecipeDetail
 }

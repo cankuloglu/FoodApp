c2f712f31bc4e874c6806a4d7c731be97724c08b
Can Kuloğlu <can.kuloglu@bahcesehir.edu.tr>
2025-06-02 22:05:44 +0300

    Implemented the FavoritesScreen

--- app/src/main/java/com/example/foodapp/presentation/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/example/foodapp/presentation/favorites/FavoritesScreen.kt
@@ -1,25 +1,164 @@
 package com.example.foodapp.presentation.favorites
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.SentimentDissatisfied
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.cankuloglu.myapplication.R
 
 @Composable
-fun FavoritesScreen(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.fillMaxSize().background(Color.White)
+fun FavoritesScreen(
+    modifier: Modifier = Modifier,
+    favoritesViewModel: FavoritesViewModel = hiltViewModel(),
+    onRecipeClick: (Int) -> Unit
+) {
+    val favorites by favoritesViewModel.favoriteRecipes.collectAsState(initial = emptyList())
+
+    Card(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .shadow(4.dp, shape = MaterialTheme.shapes.medium, clip = true),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFE8F5E9))
     ) {
-        Text(
-            text = "FAVORITES SCREEN",
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            modifier = Modifier.align(Alignment.Center)
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(vertical = 12.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = null,
+                tint = Color.Red,
+                modifier = Modifier.size(20.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = stringResource(R.string.your_favorite_recipes),
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            )
+        }
+
+
+        if (favorites.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        imageVector = Icons.Filled.SentimentDissatisfied,
+                        contentDescription = null,
+                        tint = Color.Gray,
+                        modifier = Modifier.size(48.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = stringResource(R.string.you_don_t_have_any_favorite_recipes),
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            color = Color.Gray,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Medium
+                        )
+                    )
+                }
+            }
+
+
+        } else {
+            LazyColumn(
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            ) {
+                items(favorites) { recipe ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                            .shadow(4.dp, shape = MaterialTheme.shapes.medium, clip = true)
+                            .clickable { onRecipeClick(recipe.id) }
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxWidth()
+                        ) {
+                            AsyncImage(
+                                model = recipe.image,
+                                contentDescription = recipe.title,
+                                modifier = Modifier.size(80.dp),
+                                contentScale = ContentScale.Crop
+                            )
+
+                            Spacer(modifier = Modifier.width(16.dp))
+
+                            Text(
+                                text = recipe.title,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.weight(1f)
+                            )
+
+                            Spacer(modifier = Modifier.width(16.dp))
+
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = stringResource(R.string.delete_from_favorites),
+                                modifier = Modifier
+                                    .size(25.dp)
+                                    .clickable {
+                                        favoritesViewModel.removeFavoriteRecipe(recipe)
+                                    }
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+

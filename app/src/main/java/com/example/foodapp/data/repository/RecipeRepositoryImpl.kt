fb705334f3aabf150b3fa6418ee86daaa9637c1a
Can Kuloğlu <can.kuloglu@bahcesehir.edu.tr>
2025-06-02 22:05:05 +0300

    added functions related to room (delete, get, add)

--- app/src/main/java/com/example/foodapp/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/example/foodapp/data/repository/RecipeRepositoryImpl.kt
@@ -1,16 +1,22 @@
 package com.example.foodapp.data.repository
 
+import com.example.foodapp.RecipeDao
 import com.example.foodapp.data.remote.SpoonacularApiService
 import com.example.foodapp.data.dto.toDomain
 import com.example.foodapp.domain.model.Recipe
 import com.example.foodapp.domain.model.RecipeDetail
 import com.example.foodapp.domain.repository.RecipeRepository
 import com.example.foodapp.domain.util.Constants
+import com.example.foodapp.toDomain
+import com.example.foodapp.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RecipeRepositoryImpl@Inject constructor(
     private val api: SpoonacularApiService,
-    private val apiKey: String
+    private val apiKey: String,
+    private val recipeDao: RecipeDao
 ) : RecipeRepository {
     override suspend fun getRandomRecipes(): List<Recipe> {
         val response = api.getRandomRecipes(apiKey, Constants.RANDOM_RECIPE_QUANTITY)
@@ -30,5 +36,20 @@ class RecipeRepositoryImpl@Inject constructor(
         val responseDto = api.getRecipeInformation(id,apiKey,false)
         return responseDto.toDomain()
     }
+
+    override suspend fun addFavoriteRecipe(recipe: Recipe) {
+        recipeDao.insertRecipe(recipe.toEntity())
+    }
+
+    override fun getFavoriteRecipes(): Flow<List<Recipe>> {
+        return recipeDao.getAllFavoriteRecipes().map { list ->
+            list.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun deleteRecipes(recipe: Recipe) {
+        recipeDao.deleteRecipe(recipe.toEntity())
+    }
+
 
 }
--- app/src/main/java/com/example/foodapp/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/foodapp/domain/repository/RecipeRepository.kt
@@ -2,10 +2,14 @@ package com.example.foodapp.domain.repository
 
 import com.example.foodapp.domain.model.Recipe
 import com.example.foodapp.domain.model.RecipeDetail
+import kotlinx.coroutines.flow.Flow
 
 
 interface RecipeRepository {
     suspend fun getRandomRecipes(): List<Recipe>
     suspend fun searchRecipes(query: String): List<Recipe>
     suspend fun getRecipeDetail(id:Int): RecipeDetail
+    suspend fun addFavoriteRecipe(recipe: Recipe)
+    fun getFavoriteRecipes(): Flow<List<Recipe>>
+    suspend fun deleteRecipes(recipe: Recipe)
 }

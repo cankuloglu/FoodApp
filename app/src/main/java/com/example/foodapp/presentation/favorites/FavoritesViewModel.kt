9ecb460913e9ece1c495dca61de7feb24be41b78
Can Kuloğlu <can.kuloglu@bahcesehir.edu.tr>
2025-06-12 15:40:27 +0300

    uses togglefavoritesusecase from now on

--- app/src/main/java/com/example/foodapp/presentation/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/foodapp/presentation/favorites/FavoritesViewModel.kt
@@ -2,9 +2,9 @@ package com.example.foodapp.presentation.favorites
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.foodapp.domain.usecase.DeleteFavoriteRecipeUseCase
 import com.example.foodapp.domain.usecase.GetFavoriteRecipesUseCase
-import com.example.foodapp.domain.model.Recipe
+import com.example.foodapp.domain.model.RecipeDomainModel
+import com.example.foodapp.domain.usecase.ToggleFavoriteRecipeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,24 +14,27 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FavoritesViewModel @Inject constructor(
-    private val deleteFavoriteRecipeUseCase: DeleteFavoriteRecipeUseCase,
-    private val getFavoriteRecipesUseCase: GetFavoriteRecipesUseCase
+    private val getFavoriteRecipesUseCase: GetFavoriteRecipesUseCase,
+    private val toggleFavoriteRecipeUseCase: ToggleFavoriteRecipeUseCase
 ): ViewModel()
 {
-    private val _favoriteRecipes = MutableStateFlow<List<Recipe>>(emptyList())
-    val favoriteRecipes: StateFlow<List<Recipe>> = _favoriteRecipes.asStateFlow()
+    private val _favoriteRecipes = MutableStateFlow<List<RecipeDomainModel>>(emptyList())
+    val favoriteRecipes: StateFlow<List<RecipeDomainModel>> = _favoriteRecipes.asStateFlow()
+
 
     init {
         viewModelScope.launch {
-            getFavoriteRecipesUseCase().collect {
+            getFavoriteRecipesUseCase.invokeStream(Unit).collect {
                 _favoriteRecipes.value = it
             }
         }
     }
 
-    fun removeFavoriteRecipe(recipe: Recipe) {
+    fun removeFavoriteRecipe(recipe: RecipeDomainModel) {
         viewModelScope.launch {
-            deleteFavoriteRecipeUseCase(recipe)
+            toggleFavoriteRecipeUseCase(recipe)
+
         }
     }
+
 }

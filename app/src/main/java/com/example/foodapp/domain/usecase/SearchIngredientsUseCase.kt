b6d195a79109095d4bea98c242e19d7a4767ee2c
Can Kuloğlu <can.kuloglu@bahcesehir.edu.tr>
2025-06-25 14:41:45 +0300

    searching ingredients use case

---
+++ app/src/main/java/com/example/foodapp/domain/usecase/SearchIngredientsUseCase.kt
@@ -0,0 +1,23 @@
+package com.example.foodapp.domain.usecase
+
+import com.example.foodapp.domain.model.IngredientDomainModel
+import com.example.foodapp.domain.repository.RecipeRepository
+import com.example.foodapp.domain.util.ResponseState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class SearchIngredientsUseCase@Inject constructor(
+    private val repository: RecipeRepository
+): BaseUseCase<String, ResponseState<List<IngredientDomainModel>>>(){
+
+    override fun invokeStream(param: String): Flow<ResponseState<List<IngredientDomainModel>>> = flow {
+        emit(ResponseState.Loading())
+        try {
+            val ingredients = repository.searchIngredient(param)
+            emit(ResponseState.Success(ingredients))
+        } catch (e: Exception) {
+            emit(ResponseState.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        }
+    }
+}
